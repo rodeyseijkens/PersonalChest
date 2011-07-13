cf9b793d43871d90f6e23763d2271a1fecf97ad9
rodeyseijkens <me@rodey.nl>
2011-07-12 19:01:02 -0700

    Added large chest support.

--- src/nl/rodey/personalchest/pchestInventoryListener.java
+++ src/nl/rodey/personalchest/pchestInventoryListener.java
@@ -1,60 +1,99 @@
 package nl.rodey.personalchest;
 
-import java.io.File;
-
-import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Type;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerListener;
 import org.bukkit.inventory.ItemStack;
-import org.bukkitcontrib.event.inventory.InventoryCloseEvent;
-import org.bukkitcontrib.event.inventory.InventoryListener;
+import org.bukkit.plugin.PluginManager;
 
-public class pchestInventoryListener extends InventoryListener {	
+public class pchestListener extends PlayerListener {	
+	private PluginManager pm;
     private final pchestMain plugin;
 	private pchestManager chestManager;
 	
     public ItemStack[] chestContents=null;
-
-	public pchestInventoryListener(pchestMain plugin, pchestManager chestManager) {
-		this.plugin = plugin;
+    
+	public pchestListener(pchestMain plugin, pchestManager chestManager) {
+        this.plugin = plugin;
 		this.chestManager = chestManager;
 	}
+	
 
-	@Override
-	public void onInventoryClose(InventoryCloseEvent event) {
+	public void registerEvents()
+    {
+        pm = plugin.getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_INTERACT, this, Event.Priority.Normal, plugin);
+		pm.registerEvent(Type.CUSTOM_EVENT, plugin.inventoryListener, Event.Priority.Normal, plugin);
+    }
+    
+    public void onPlayerInteract(PlayerInteractEvent event)
+    {
+    	
+        Block block = event.getClickedBlock();
+        if (block == null) return; 
+        
+        boolean cancel=false;
+        
+        
+	        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+	        {
+	        	if(block.getType().equals(Material.CHEST)) 
+	            {
+		        	if(chestManager.checkChestRegisterd(block))
+		        	{
+		                cancel = onChestInteract(block,event.getPlayer());
+		    		}
+		    		else
+		    		{
+		    			cancel = false;
+		    		}
+	            } 
+	            
+	            if(cancel) event.setCancelled(true);
+	        }   
+	        else if(event.getAction().equals(Action.LEFT_CLICK_BLOCK))
+	        {	
+	            if(block.getType().equals(Material.CHEST))
+	            {
+	            	if(chestManager.checkChestRegisterd(block))
+	            	{
+	            		cancel = true;
+	            		event.getPlayer().sendMessage("[PersonalChest] This chest is protected.");
+	            		
+	                    if(cancel) event.setCancelled(true); 
+	            	}
+	            }
+	        } 
+        
+        if(cancel) event.setCancelled(true);    		
+    }
+    
+    private boolean onChestInteract(Block block, Player player)
+    {
+    	
+        // By default we cancel access to treasure chests
+    	boolean cancel = true;
 		
-		if(event.getLocation() != null)
+		Chest chest = (Chest) block.getState();
+		
+		if(chestManager.checkChestOpened(block))
 		{
-			String locX = String.format("%.0f", event.getLocation().getX());			
-			String locY = String.format("%.0f", event.getLocation().getY());		
-			String locZ = String.format("%.0f", event.getLocation().getZ());
-			
-			String blockFilename = locX + "_" + locY + "_" + locZ;
-			String blockWorldName = event.getLocation().getWorld().getName();
-			String playerName = event.getPlayer().getName();
-			
-			Location blockLoc = event.getLocation();
-			
-			Block block = blockLoc.getBlock();
-
-			Inventory inv = event.getInventory();
-	        chestContents = inv.getContents();
-			
-			File worldDataFolder = new File(plugin.getDataFolder().getAbsolutePath(), "chests" + File.separator + "Worlds" + File.separator + blockWorldName);
-			File chestFile = new File(worldDataFolder , blockFilename + ".chest");
-				
-			if (!chestFile.exists())
-			{
-				return;
-			}
-			else
-			{				
-				chestManager.createPersonal(playerName, chestContents, block);
-
-				chestManager.removeChestOpened(block);
-			}
-			
-			
+			cancel = true;
+    		player.sendMessage("[PersonalChest] Chest is currently in use.");
 		}
-	}
+		else if(chestManager.load(player, chest, block))
+		{
+			chestManager.setChestOpened(block, player);
+			cancel = false;
+		}	
+			
+        return cancel;
+    }
+    
 }
