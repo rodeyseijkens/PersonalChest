2a3cedebbee514cd5ae674d919a99310e1fe1284
rodeyseijkens <me@rodey.nl>
2011-07-16 07:40:32 -0700

    Added BukkitContrib Auto-download.

--- src/nl/rodey/personalchest/pchestMain.java
+++ src/nl/rodey/personalchest/pchestMain.java
@@ -1,11 +1,17 @@
 package nl.rodey.personalchest;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 import com.nijiko.permissions.PermissionHandler;
@@ -28,7 +34,18 @@ public class pchestMain extends JavaPlugin {
 	public void onEnable() {
 		
 		log.info("["+getDescription().getName()+"] version "+getDescription().getVersion()+" loading...");
-				
+		
+		log = getServer().getLogger();
+	    final PluginManager pm = getServer().getPluginManager();
+	    if (pm.getPlugin("BukkitContrib") == null)
+        try {
+        	downloadBukkitContrib(log, new URL("http://bit.ly/autoupdateBukkitContrib"), new File("plugins/BukkitContrib.jar"));
+            pm.loadPlugin(new File("plugins" + File.separator + "BukkitContrib.jar"));
+            pm.enablePlugin(pm.getPlugin("BukkitContrib"));
+        } catch (final Exception ex) {
+            log.warning("[LogBlock] Failed to install BukkitContrib, you may have to restart your server or install it manually.");
+        }
+	
         // Load configuration
         loadConfig();
        
@@ -129,5 +146,31 @@ public class pchestMain extends JavaPlugin {
 	public boolean checkpermissions(Player player, String string, Boolean standard)
 	{
 		return ( (player.isOp() == true) || (usingpermissions ? Permissions.has(player,string) : standard));
+	}
+	
+	private static void downloadBukkitContrib(Logger log, URL url, File file) throws IOException {
+	    if (!file.getParentFile().exists())
+	        file.getParentFile().mkdir();
+	    if (file.exists())
+	        file.delete();
+	    file.createNewFile();
+	    final int size = url.openConnection().getContentLength();
+	    log.info("Downloading " + file.getName() + " (" + size / 1024 + "kb) ...");
+	    final InputStream in = url.openStream();
+	    final OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+	    final byte[] buffer = new byte[1024];
+	    int len, downloaded = 0, msgs = 0;
+	    final long start = System.currentTimeMillis();
+	    while ((len = in.read(buffer)) >= 0) {
+	        out.write(buffer, 0, len);
+	        downloaded += len;
+	        if ((int)((System.currentTimeMillis() - start) / 500) > msgs) {
+	            log.info((int)((double)downloaded / (double)size * 100d) + "%");
+	            msgs++;
+	        }
+	    }
+	    in.close();
+	    out.close();
+	    log.info("Download finished");
 	}
 }
